f324763729396b341055ef097a63a8bb7bf71e73
mir809 <humir809@naver.com>
2023-04-18 06:28:57 +0900

    #3.4 Recap

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const PORT = 4000;
 const app = express();
 
 const handleHome = (req, res) => {
-  return res.send("This my Home");
+  return res.send("<h1>This my Home</h1>");
 };
 const handleLogin = (req, res) => {
-  return res.send("Login here");
+  return res.send({ message: "Login here" });
 };
 
 app.get("/", handleHome);
