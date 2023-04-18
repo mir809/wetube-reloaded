ae424b21bfbadaca4cbb4fcb66e7117cd40ccdc1
mir809 <humir809@naver.com>
2023-04-18 16:01:49 +0900

    #3.6 Middlewares part Two (최대)

--- src/server.js
+++ src/server.js
@@ -4,18 +4,34 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to : ${req.url} `);
+const loggerMid = (req, res, next) => {
+  console.log(`${req.method} ${req.url} `);
+  next();
+};
+
+const privateMid = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue.");
   next();
 };
 
 const handleHome = (req, res) => {
-  return res.end();
+  return res.send("I love middlewares");
 };
 
-app.get("/", gossipMiddleware, handleHome);
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge.");
+};
+
+app.use(loggerMid);
+app.use(privateMid);
+
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
-
 app.listen(PORT, handleListening);
