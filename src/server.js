0b9afb30406f1113b3063850a3671e9a73ee05b5
mir809 <humir809@naver.com>
2023-04-18 16:10:58 +0900

    #3.6 Middlewares part Two (마무리)

--- src/server.js
+++ src/server.js
@@ -9,28 +9,11 @@ const loggerMid = (req, res, next) => {
   next();
 };
 
-const privateMid = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed, you may continue.");
-  next();
-};
-
 const handleHome = (req, res) => {
   return res.send("I love middlewares");
 };
 
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the private lounge.");
-};
-
-app.use(loggerMid);
-app.use(privateMid);
-
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+app.get("/", loggerMid, handleHome);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
