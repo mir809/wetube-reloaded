1f38941f9269f7168266eec2a32064dfb2398a4e
mir809 <humir809@naver.com>
2023-04-21 01:33:15 +0900

    #4.7 URL Parameters part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,6 +4,14 @@ export const search = (req, res) => res.send("Search");
 
 //videos
 export const upload = (req, res) => res.send("Upload Video");
-export const watch = (req, res) => res.send("Watch Video");
-export const edit = (req, res) => res.send("Edit Video");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+
+export const watch = (req, res) => {
+  return res.send(`Watch Video #${req.params.id}`);
+};
+export const edit = (req, res) => {
+  res.send("Edit Video");
+};
+
+export const deleteVideo = (req, res) => {
+  res.send("Delete Video");
+};
