142a957c3a4db2f4aa5fc48d78518c24990d0122
mir809 <humir809@naver.com>
2023-04-20 23:46:22 +0900

    #4.3 Exports

---
+++ src/controllers/videoController.js
@@ -0,0 +1,4 @@
+export const trending = (req, res) => res.send("Home Page Vide");
+
+export const watch = (req, res) => res.send("Watch Video");
+export const edit = (req, res) => res.send("Edit Video");
