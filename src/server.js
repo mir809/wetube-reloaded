b179932a5ea9142e45656abdaee53eded9f23a8d
mir809 <humir809@naver.com>
2023-04-20 22:58:48 +0900

    #4.2 Cleaning the Code

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import morgan from "morgan";
 
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+
 const PORT = 4000;
 
 const app = express();
@@ -8,39 +12,11 @@ const loggerMid = morgan("dev");
 
 app.use(loggerMid);
 
-const globalRouter = express.Router();
-const handleHome = (req, res) => res.send("Home");
-
-globalRouter.get("/", handleHome);
-
-const userRouter = express.Router();
-const handleEditUser = (req, res) => res.send("Edit User");
-
-userRouter.get("/edit", handleEditUser);
-
-const videoRouter = express.Router();
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-
-videoRouter.get("/watch", handleWatchVideo);
-
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const home = (req, res) => {
-  console.log(`Response complete`);
-  return res.send(
-    "Welcome my home <a href=http://localhost:4000/login>go to Log-in</a>"
-  );
-};
-
-const login = (req, res) => {
-  console.log(`Response complete`);
-  return res.send(
-    "It's Log-in page <a href=http://localhost:4000/>back to home</a>"
-  );
-};
-
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
+
 app.listen(PORT, handleListening);
