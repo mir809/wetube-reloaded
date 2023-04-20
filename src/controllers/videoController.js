752b266cf3ef276798cd8be5fbfd24fdd000c9e4
mir809 <humir809@naver.com>
2023-04-21 07:20:01 +0900

    #5.4 Variables to Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,12 @@
 // home(/)
-export const home = (req, res) => res.render("home");
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
 export const search = (req, res) => res.send("Search");
 
 //videos
 export const upload = (req, res) => res.send("Upload Video");
 
-export const watch = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
+export const watch = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 
 export const deleteVideo = (req, res) => {
   res.send("Delete Video");
