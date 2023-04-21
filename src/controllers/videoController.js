7b834cc0dbb072f7f97b7eb518d96b5204d42519
mir809 <humir809@naver.com>
2023-04-22 03:03:32 +0900

    #5.9 Mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,31 @@
 // home(/)
 export const home = (req, res) => {
-  const videos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2min ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2min ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2min ago",
+      views: 59,
+      id: 1,
+    },
+  ];
   res.render("home", { pageTitle: "Home", videos });
 };
 
