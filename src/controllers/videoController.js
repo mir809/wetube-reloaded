e970d7446759f5beb45bca9ba007c5d8c168ad3e
mir809 <humir809@naver.com>
2023-04-22 04:13:33 +0900

    #6.1 Array Database part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,7 @@ let videos = [
     rating: 4,
     comments: 7,
     createdAt: "2min ago",
-    views: 25,
+    views: 57,
     id: 1,
   },
   {
@@ -36,7 +36,7 @@ export const upload = (req, res) => res.send("Upload Video");
 export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}` });
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const deleteVideo = (req, res) => res.send("Delete Video");
