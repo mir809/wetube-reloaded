cfeab1127d7913ac9a8a1633dd11f978ee5fc94d
mir809 <humir809@naver.com>
2023-04-22 01:31:15 +0900

    #5.8 Iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,19 +1,13 @@
-const fakeUser = {
-  username: "Mir",
-  loggedIn: false,
+// home(/)
+export const home = (req, res) => {
+  const videos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  res.render("home", { pageTitle: "Home", videos });
 };
 
-// home(/)
-export const home = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser });
 export const search = (req, res) => res.send("Search");
 
 //videos
 export const upload = (req, res) => res.send("Upload Video");
-
 export const watch = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
-
-export const deleteVideo = (req, res) => {
-  res.send("Delete Video");
-};
+export const deleteVideo = (req, res) => res.send("Delete Video");
