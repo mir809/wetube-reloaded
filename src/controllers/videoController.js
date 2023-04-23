1e8df84b9cb53e54df3cca4b44b686848c84f6c4
mir809 <humir809@naver.com>
2023-04-24 08:50:58 +0900

    #6.11 ~ 12 Our First Query (!! 에러)
    !! 2023.04.06 기준, model.find()에서 콜백함수 사용중지하는 것으로 몽구스가 업데이트.
    
    => 강의 영상처럼 제대로 실행 안됨 (이해)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,51 +1,25 @@
-let videos = [
-  {
-    title: "First Video",
-    rating: 4,
-    comments: 7,
-    createdAt: "2min ago",
-    views: 57,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 4,
-    comments: 2,
-    createdAt: "2min ago",
-    views: 59,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 4,
-    createdAt: "2min ago",
-    views: 36,
-    id: 3,
-  },
-];
+import Video from "../models/Video";
 
 // home(/)
 export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos });
+  Video.find({}, (error, videos) => {
+    res.render("home", { pageTitle: "Home", videos: [] });
+  });
 };
 
 //videos
 export const watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
+  return res.render("watch", { pageTitle: `Watching: ` });
 };
 
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+  return res.render("edit", { pageTitle: `Editing: ` });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
 
@@ -54,14 +28,5 @@ export const getUpload = (req, res) => {
 };
 export const postUpload = (req, res) => {
   const { title } = req.body;
-  const newVideo = {
-    title,
-    rating: 0,
-    comments: 0,
-    createdAt: "just now",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
   return res.redirect("/");
 };
