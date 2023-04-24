c0d0e004ccb0f2635f82176df8a532a4a03d8a11
mir809 <humir809@naver.com>
2023-04-24 10:25:12 +0900

    #6.15 Creating a Video part One
    !! packge.json 파일 변경 사항은 #6.11 Our First Query에서 적용된 부분

--- src/models/Video.js
+++ src/models/Video.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const videoSchema = new mongoose.Schema({
   title: String,
   description: String,
-  CreatedAt: Date,
+  createdAt: Date,
   hashtags: [{ type: String }],
   meta: {
     views: Number,
