bf3149f0ea4b33035ac29d02b87f1a054bec730a
mir809 <humir809@naver.com>
2023-04-24 10:54:26 +0900

    #6.16 Creating a Video part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -11,7 +11,8 @@ Video.find({}, (error, videos) => {
 // home(/)
 export const home = async (req, res) => {
   const videos = await Video.find({});
-  return res.render("home", { pageTitle: "Home", videos: [] });
+  console.log(videos);
+  return res.render("home", { pageTitle: "Home", videos });
 };
 
 //videos
@@ -33,9 +34,9 @@ export const postEdit = (req, res) => {
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  const video = new Video({
+  await Video.create({
     title,
     description,
     createdAt: Date.now(),
@@ -45,6 +46,5 @@ export const postUpload = (req, res) => {
       rating: 0,
     },
   });
-  console.log(video);
   return res.redirect("/");
 };
