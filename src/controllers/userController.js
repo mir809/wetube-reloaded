58e298ccf63e8477ff01152b22d6198fa349f43a
mir809 <humir809@naver.com>
2023-04-28 10:17:47 +0900

    #7.18 Github Login part Three
    깃허브를 통해 로그인(권한 부여) 한 뒤 깃허브에서 받은 코드를 Access토큰으로 바꿔줘야 한다.
    
    이를 위해서 필요한 3가지는
    
    1. 깃허브에 서 받은 code
    2. 클라이언트 ID
    3. 클라이언트 Secrets
    
    이중 코드는 깃허브 연동 후 URL에 표시되고 클라이언트 Secrets 는 사용중인 OAuth Apps에서 사용 가능하다.
    또한  Secrets 은 말그대로 노출이 되면 안되기 때문에 env파일을 통해 숨겨줘야 한다.
    
    -----------------------------------------
    이들을 조합해 url을 생성한 후 해당 url에서 fetch를 사용해 깃허브 서버에서 데이터를 가져온다. => fetch([조합한 URL주소])
    
    ! fetch 설명 (바닐라 JS강의 내용)
    
    1. fetch('url')를 통해 다른 서버에서 데이터를 가져올 수 있다.
    하지만 res.body 에 담겨있는 날것의 url만으로는 제대로 된 객체를 받아올 수 없다.
    
    2. 때문에 추가적으로 'json()'함수를 작성하여 다른 서버에서 데이터를 object형식으로 받아올 수 있다.
    (response의 스트림을 가져와 끝까지 읽고, res.body의 텍스트를 promise의 형태로 반환한다.)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -65,7 +65,7 @@ export const postLogin = async (req, res) => {
 export const startGithubLogin = (req, res) => {
   const baseUrl = `https://github.com/login/oauth/authorize`;
   const config = {
-    client_id: "52056a3f1b483d94f128",
+    client_id: process.env.GH_GLIENT,
     allow_signup: false,
     scope: "read:user user:email",
   };
@@ -74,7 +74,24 @@ export const startGithubLogin = (req, res) => {
   return res.redirect(finalUrl);
 };
 
-export const finishGithubLogin = (req, res) => {};
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = `https://github.com/login/oauth/access_token`;
+  const config = {
+    client_id: process.env.GH_GLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const data = await fetch(finalUrl, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const json = await data.json();
+  console.log(json);
+};
 
 //users
 export const edit = (req, res) => res.send("Edit User");
