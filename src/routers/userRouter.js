8e20cba5b3f330de47bb6c2fd68b5c22b1b325f8
mir809 <humir809@naver.com>
2023-04-28 09:19:16 +0900

    #7.17 Github Login part Two
    깃허브와 연동되는 URL뒤에 scope를 붙임으로써 얼마나 더 많은 정보를 읽어내고 가져올지 설정할 수 있다.
    
    원하는 설정을 URL뒤 '&scope=[옵션]'형식으로 써주고 여러 옵션들을 적용하고 싶은 경우 옵션 사이에 띄어쓰기를 해주면 된다.
    
    ! scope옵션 예 ('&scope='뒤에 붙임)
    
    - user  = 유저의 모든 정보를 읽거나 쓸수 있다.
    
    - read:user = 유저의 모든 정보를 읽기만 가능
    
    - user:email = 유저의 이메일을 읽을 수 있다.
    
    !! 코드 작성시 URL을 원하는 위치에 그대로 붙여주는게 아니라 따로 라우터, 컨트롤러를 만들어서 해당 위치로 향하도록 설정 (코드 참조)
    
    ------------------------------------------
    
    Github연동 페이지에서 'Authorize'버튼을 누를경우  'OAuth Apps'생성시 설정했던 'Authorization callback URL'로 이동하게 된다. (변경 가능)

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,12 +1,23 @@
 import express from "express";
 
-import { edit, remove, logout, profile } from "../controllers/userController";
+import {
+  edit,
+  remove,
+  logout,
+  profile,
+  startGithubLogin,
+  finishGithubLogin,
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/delete", remove);
+
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
+
 userRouter.get("/:id(\\d+)", profile);
 
 export default userRouter;
