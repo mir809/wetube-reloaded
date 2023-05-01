3b484fc7bef8118f5977fd1dee228d22de3d7bf7
mir809 <humir809@naver.com>
2023-05-01 12:36:57 +0900

    #9.1 Webpack Configuration part One
    Webpack 사용 이유
    : 우리가 코드를 짤 때 사람이 보기 좋으면 좋을 수록 호환성이 떨어지는 등의 문제가 생길 수 있다.
    (사람이 보기좋음 = 기계어와 거리가 멀음 = 기계가 이해하기 힘듬 = 호환성 떨어짐)
    때문에 기계가 이해하기 힘든 코드를 기계가 이해하기 쉽고 호환이 잘 될수 있도록 못생긴 모습의 js코드로 변환해주기 위한 도구이다.
    
    Webpack 사용 방법
    
    1. webpack 설치 :
     npm i webpack webpack-cli -D
    (webpack, webpack-cli 패키지를 devDependencies로 설치)
    
    2. webpack.config.js파일 생성
    : Webpack이 읽을 configuraion파일 (=설정 파일)
    -webpack은 오래된 자바스크립트 코드만 이해할 수 있음
    (!! 오래됐다 = 구식 = 예쁘지않음 = 비교적 기계어에 가까움 = 호환성이 높다. )
    
    3. webpack필수 설정요소 2가지
    : entry, output(이름 고정)
    
    3-1. entry : 우리가 webpack을 통해 변경하고자 하는 파일.
    (사람이 보기 좋은 상태의 코드 => 넣어줄 코드 = 입력 )
    
    - entry파일의 경로(위치) 설정필요
    
    3-2. output : webpack을 통해압축하고 최소화 시킨 js코드
    (사람이 보기 안좋은 코드 => webpack 결과물 = 출력)
    
    -전환 후 파일의 이름 설정(filename)
    (변경전과 동일해도 상관 X)
    -전환한 파을 저장할 경로 설정(path)

---
+++ src/client/js/main.js
@@ -0,0 +1,6 @@
+const hello = async () => {
+  alert("hi! Hello");
+  const x = await fetch("");
+};
+
+hello();
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  entry: "./src/client/js/main.js",
+  output: {
+    fielname: "main.js",
+    path: "./assets/js",
+  },
+};
