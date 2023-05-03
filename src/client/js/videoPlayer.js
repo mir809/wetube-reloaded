67af805dbcdcf6c4a9af361339f10287844d7912
mir809 <humir809@naver.com>
2023-05-03 22:02:01 +0900

    #11.3 Volume
    1. 볼륨 막대 조절시 실제 볼륨 실시간 반영
    
    2. 볼륨 막대 조절 후 음소거 => 음소거 해제시 고정된 값이 아닌 음소거 이전 조절했던 값으로 돌아오도록 설정
    
    !! 추가(강의내용 +)
    
    - 음소거 버튼이 아닌 볼륨막대조절로 소리를 0으로 만들었을 때 '음소거' => '음소거 해제' 로 바뀌도록 변경
    
    - 볼륨 막대로 소리를 0으로 만든 뒤 '음소거 해제' 클릭시 가장 최근에 설정했던 소리가 되도록 변경("chage"이용)

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,10 +2,14 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
+
+let volumeValue = 0.5;
+
+video.volume = volumeValue; // 영상의 실제 소리
 
 //  플레이/일시정지 버튼
-const handlePlayBtn = (e) => {
+const clickPlayBtn = () => {
   if (video.paused) {
     video.play();
   } else {
@@ -15,15 +19,43 @@ const handlePlayBtn = (e) => {
 };
 
 // 음량 - 음소거 버튼, 볼륨 막대
-const handleMute = (e) => {
+const clickMuteBtn = () => {
   if (video.muted) {
     video.muted = false;
+    video.volume = volumeValue;
   } else {
     video.muted = true;
   }
   muteBtn.innerText = video.muted ? "음소거 해제" : "음소거";
-  volume.value = video.muted ? 0 : 0.5;
+  volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
-playBtn.addEventListener("click", handlePlayBtn);
-muteBtn.addEventListener("click", handleMute);
+const volumeInput = (event) => {
+  // 볼륨조절 드래그시 실시간 적용
+  const {
+    target: { value }, // 볼륨 막대를 조절한 값
+  } = event;
+  if (Number(value) === 0) {
+    muteBtn.innerText = "음소거 해제";
+    video.muted = true;
+  } else {
+    video.muted = false;
+    muteBtn.innerText = "음소거";
+  }
+  video.volume = value; // 영상의 실제 소리
+};
+
+const volumeChange = (event) => {
+  // 볼륨조절 드래그 후 마우스를 놨을 때만 적용
+  const {
+    target: { value }, // 볼륨 막대를 조절한 값
+  } = event;
+  if (Number(value) !== 0) {
+    volumeValue = value;
+  }
+};
+
+playBtn.addEventListener("click", clickPlayBtn);
+muteBtn.addEventListener("click", clickMuteBtn);
+volumeRange.addEventListener("input", volumeInput);
+volumeRange.addEventListener("change", volumeChange);
