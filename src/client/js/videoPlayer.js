cf067ffb58cb8c2055ee4f17c098154a44b77313
mir809 <humir809@naver.com>
2023-05-03 20:05:31 +0900

    #11.1 Play Pause
    동영상 하단에 플레이/일시정지 버튼, 음소거 버튼, 볼륨 막대 , 동영상 시간 생성
    
    플레이/일시정지 버튼 동작

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,24 @@
-console.log("Video Plaeyr");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayBtn = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "Pause");
+
+const handleMute = (e) => {};
+
+playBtn.addEventListener("click", handlePlayBtn);
+muteBtn.addEventListener("click", handleMute);
+
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
