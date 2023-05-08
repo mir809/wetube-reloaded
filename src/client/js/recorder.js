27a2f5d2f5f9508e9f5ad70e9f3f2c43aee0f59a
mir809 <humir809@naver.com>
2023-05-09 03:49:07 +0900

    #13.4 Downloading the File
    녹화된 파일 다운로드 기능 완성

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -2,13 +2,22 @@ const record_Btn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 const download_Btn = document.getElementById("download");
 
-let stream;
-let recorder;
+let stream; // 컴퓨터 카메라로 촬영 (실시간으로 보기만)
+let recorder; // 녹화 시작
+let videoFile; // 녹화된 파일
 
-const download = () => {};
+const download = () => {
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "MyRecoding.webm";
+  //링크 클릭시 주소이동이 아닌, 다운로드
+  document.body.appendChild(a);
+  //링크를 클릭하기위해 실제 html body에 추가
+  a.click(); // html a태그(링크)를 사용자가 직접누른것과 똑같이 동작
+};
 
 const record_Stop = () => {
-  record_Btn.innerText = "Restart Recording";
+  record_Btn.innerText = "다시 녹화하기";
   record_Btn.removeEventListener("click", record_Stop);
   record_Btn.addEventListener("click", record_Start);
 
@@ -16,7 +25,7 @@ const record_Stop = () => {
 };
 
 const record_Start = () => {
-  record_Btn.innerText = "Stop Recording";
+  record_Btn.innerText = "녹화 종료";
   record_Btn.removeEventListener("click", record_Start);
   record_Btn.addEventListener("click", record_Stop);
 
@@ -25,7 +34,7 @@ const record_Start = () => {
 
   recorder.ondataavailable = (event) => {
     //녹화 종료시 자동 발생되는 이벤트
-    const videoFile = URL.createObjectURL(event.data);
+    videoFile = URL.createObjectURL(event.data);
     // 녹화된 데이터를 웹사이트가 아닌 브라우저의 메모리에 할당
     //(실제 인터넷 주소가아닌 브라우저상에 존재하는 파일을 가리킴)
     video.srcObject = null;
@@ -34,9 +43,10 @@ const record_Start = () => {
     video.play();
   };
 
-  recorder.start(); //실제 촬영 시작
+  recorder.start(); //실제 녹화 시작
   video.srcObject = stream;
   video.play();
+  // 녹화 재시작지 현재 카메라에 보이는 모습 표시
 };
 
 const init = async () => {
@@ -47,7 +57,7 @@ const init = async () => {
   });
   video.srcObject = stream;
   video.play();
-  // 페이지 들어오자 마자 영상 촬영 미리보기 켜짐
+  // 페이지 들어오자 마자 영상 촬영 미리보기 시작
 };
 
 init();
