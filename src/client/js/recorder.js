ca0250b1b940ba87c4d0922562b8c3f7d023fc52
mir809 <humir809@naver.com>
2023-05-09 00:57:36 +0900

    #13.1 Video Preview
    촬영중인 동영상 실시간으로 재생(화면에 표시)

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,14 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
 
 const recordStart = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
+    audio: false,
     video: true,
+    //{ width: 500, height: 300 } - 촬영크기 설정 가능
   });
-  console.log(stream);
+  video.srcObject = stream;
+  video.play();
 };
 
 startBtn.addEventListener("click", recordStart);
