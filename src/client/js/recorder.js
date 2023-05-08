d7575f194cb5c5943961c489206da81d487e6775
mir809 <humir809@naver.com>
2023-05-09 01:52:09 +0900

    #13.2 Recording Video
    녹화 버튼 1개를 클릭할때 마다
    녹화시작/정지 반복
    
    녹화 버튼 클릭시시 실제로 녹화를 시작하고 5초후에 자동으로 종료.
    녹화 종료시 자동으로 녹화한 데이터를 받아옴

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,14 +1,48 @@
-const startBtn = document.getElementById("startBtn");
+const record_Btn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const recordStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+
+const record_Stop = () => {
+  record_Btn.innerText = "Start Recording";
+  record_Btn.removeEventListener("click", record_Stop);
+  record_Btn.addEventListener("click", record_Start);
+};
+
+const record_Start = () => {
+  record_Btn.innerText = "Stop Recording";
+  record_Btn.removeEventListener("click", record_Start);
+  record_Btn.addEventListener("click", record_Stop);
+
+  const recorder = new MediaRecorder(stream);
+
+  recorder.ondataavailable = (event) => {
+    //녹화 종료시 자동 발생되는 이벤트
+    console.log("recording done");
+    console.log(event);
+    console.log(event.data); // 녹화된 데이터
+  };
+
+  console.log(recorder); // 시작 전 - 비활성화 상태
+  recorder.start(); //실제 촬영 시작
+  console.log(recorder); // 시작 후 - 녹화중 상태
+
+  setTimeout(() => {
+    recorder.stop();
+  }, 5000); // 5초후 자동으로 촬영 종료
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: true,
     //{ width: 500, height: 300 } - 촬영크기 설정 가능
   });
   video.srcObject = stream;
   video.play();
+  // 페이지 들어오자 마자 영상 촬영 미리보기 켜짐
 };
 
-startBtn.addEventListener("click", recordStart);
+init();
+
+record_Btn.addEventListener("click", record_Start);
